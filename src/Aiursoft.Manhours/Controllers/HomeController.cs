78ca061cf7340d31f70b21cce8b7ea5aa2627cfc
AnduinXue <anduin@aiursoft.com>
2023-11-22 19:16:52 +0000

    Fix index text.

--- src/Aiursoft.Manhours/Controllers/HomeController.cs
+++ src/Aiursoft.Manhours/Controllers/HomeController.cs
@@ -6,6 +6,6 @@ public class HomeController : ControllerBase
 {
     public IActionResult Index()
     {
-        return Ok("Welcome to Aiursoft ManHours server! This link is not a web page! Please use it as a badge generator. You can access: '/gitlab.aiursoft.cn/anduin/flyclass' to try!");
+        return Ok("Welcome to Aiursoft ManHours server! This link is not a web page! Please use it as a badge generator. You can access: '/gitlab/gitlab.aiursoft.cn/anduin/flyclass' to try!");
     }
 }

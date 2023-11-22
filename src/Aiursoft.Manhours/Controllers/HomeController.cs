21b677f98b53a8947a45bbbb67c863722e5e3877
AnduinXue <anduin@aiursoft.com>
2023-11-22 19:14:20 +0000

    Add index.

---
+++ src/Aiursoft.Manhours/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aiursoft.ManHours.Controllers;
+
+public class HomeController : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return Ok("Welcome to Aiursoft ManHours server! This link is not a web page! Please use it as a badge generator. You can access: '/gitlab.aiursoft.cn/anduin/flyclass' to try!");
+    }
+}

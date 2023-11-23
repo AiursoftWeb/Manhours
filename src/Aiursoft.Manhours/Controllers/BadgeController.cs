e6fb436823ab5a19f36105a59ca5d2794909df2c
AnduinXue <anduin@aiursoft.com>
2023-11-23 13:54:19 +0000

    New path grammar.

--- src/Aiursoft.Manhours/Controllers/BadgeController.cs
+++ src/Aiursoft.Manhours/Controllers/BadgeController.cs
@@ -20,9 +20,18 @@ public class BadgeController : ControllerBase
     }
 
     [Route("gitlab/{**repo}")]
-    public async Task<IActionResult> GitLabRepo([FromRoute] string repo) // sample value: gitlab/gitlab.aiursoft.cn/anduin/flyclass
+    public async Task<IActionResult> GitLabRepo([FromRoute] string repo) 
+    // sample value: gitlab/gitlab.aiursoft.cn/anduin/flyclass.svg
+    // sample value: gitlab/gitlab.aiursoft.cn/anduin/flyclass.json
     {
-        var formattedLink = new GitLabLink(repo);
+        var extension = repo.Split('.').LastOrDefault();
+        if (string.IsNullOrEmpty(extension))
+        {
+            return NotFound();
+        }
+        
+        var repoWithoutExtension = repo.Substring(0, repo.Length - extension.Length - 1);
+        var formattedLink = new GitLabLink(repoWithoutExtension);
         var hours = await _cacheService.RunWithCache(
             $"gitlab-{formattedLink.Server}-{formattedLink.Group}-{formattedLink.Project}", async () =>
             {
@@ -47,33 +56,17 @@ public class BadgeController : ControllerBase
         //#fe7d37 Orange
         //#dfb317 Yellow
         //#4c1 Green
-        return File(badge.Draw(), "image/svg+xml");
-    }
-    
-    [Route("shields/gitlab/{**repo}")]
-    public async Task<IActionResult> ShieldsGitLabRepo([FromRoute] string repo) // sample value: shield/gitlab/gitlab.aiursoft.cn/anduin/flyclass
-    {
-        var formattedLink = new GitLabLink(repo);
-        var hours = await _cacheService.RunWithCache(
-            $"gitlab-{formattedLink.Server}-{formattedLink.Group}-{formattedLink.Project}", async () =>
-            {
-                var commits = await formattedLink.GetCommits().ToListAsync();
-                var commitTimes = commits.Select(t => t.CommittedDate).ToList();
-                var workTime = _workTimeService.CalculateWorkTime(commitTimes);
-                return workTime.TotalHours;
-            }, cachedMinutes: r => r < 100 ? TimeSpan.FromMinutes(10) : TimeSpan.FromMinutes(100));
-
-        var badge = new Badge
+        if (extension == "svg")
         {
-            Label = "man-hours",
-            Message = $"{(int)hours}",
-            Color =
-                hours < 10 ? "e05d44" :
-                hours < 30 ? "fe7d37" :
-                hours < 90 ? "dfb317" :
-                "4c1"
-        };
-
-        return this.Ok(badge);
+            return File(badge.Draw(), "image/svg+xml");
+        }
+        else if (extension == "json")
+        {
+            return Ok(badge);
+        }
+        else
+        {
+            return NotFound();
+        }
     }
 }

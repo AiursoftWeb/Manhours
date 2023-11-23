e8e0e57636b546ae12fe11f958bb7035392376f3
AnduinXue <anduin@aiursoft.com>
2023-11-23 14:32:22 +0000

    Fix tests.

--- tests/IntegrationTests/BasicTests.cs
+++ tests/IntegrationTests/BasicTests.cs
@@ -36,12 +36,42 @@ public class BasicTests
     }
 
     [TestMethod]
-    [DataRow("/gitlab/gitlab.aiursoft.cn/aiursoft/webtools")]
-    [DataRow("/gitlab/gitlab.aiursoft.cn/anduin/flyclass")]
+    [DataRow("/gitlab/gitlab.aiursoft.cn/aiursoft/webtools.svg")]
+    [DataRow("/gitlab/gitlab.aiursoft.cn/anduin/flyclass.svg")]
+    [DataRow("/gitlab/gitlab.aiursoft.cn/anduin/flyclass.json")]
     public async Task GetBadge(string url)
     {
         var response = await _http.GetAsync(_endpointUrl + url);
         response.EnsureSuccessStatusCode(); // Status Code 200-299
-        Assert.AreEqual("image/svg+xml", response.Content.Headers.ContentType?.ToString());
+        if (url.EndsWith(".json"))
+        {
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        }
+        else if (url.EndsWith(".svg"))
+        {
+            Assert.AreEqual("image/svg+xml", response.Content.Headers.ContentType?.ToString());
+        }
+        else
+        {
+            Assert.Fail();
+        }
+    }
+
+    [TestMethod]
+    [DataRow("/gitlab/gitlab.aiursoft.cn/aiursoft/webtools.ssss")]
+    [DataRow("/gitlab/gitlab.aiursoft.cn/anduin/flyclass")]
+    [DataRow("/gitlab/gitlab.aiursoft.cn/anduin")]
+    public async Task GetBadgeFailed(string url)
+    {
+        var response = await _http.GetAsync(_endpointUrl + url);
+        try
+        {
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Fail();
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
     }
 }

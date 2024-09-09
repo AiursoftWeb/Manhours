cdbf3f8107b698efb5681262b08f12e57b2373f0
Anduin Xue <anduin@aiursoft.com>
2024-09-09 08:49:18 +0000

    Add Notifications Dropdown ViewModel and package-lock.json
    Introduced a new ViewModel for notifications, including a Notification class. Also, added the package-lock.json file for wwwroot dependencies management.

--- src/Aiursoft.Manhours/Controllers/HomeController.cs
+++ src/Aiursoft.Manhours/Controllers/HomeController.cs
@@ -84,7 +84,49 @@ public class HomeController : Controller
                         Text = "View all messages"
                     }
                 },
-                NotificationsDropdown = new NotificationsDropdownViewModel(),
+                NotificationsDropdown = new NotificationsDropdownViewModel
+                {
+                    Notifications = 
+                    [
+                        new Notification
+                        {
+                            Icon = "alert-circle",
+                            IconClass = "text-danger",
+                            Title = "Server down",
+                            Message = "Server was down for 5 minutes.",
+                            TriggerTime = DateTime.Now - TimeSpan.FromMinutes(2)
+                        },
+                        new Notification
+                        {
+                            Icon="bell",
+                            IconClass = "text-primary",
+                            Title = "New user",
+                            Message = "A new user registered.",
+                            TriggerTime = DateTime.Now - TimeSpan.FromMinutes(5)
+                        },
+                        new Notification
+                        {
+                            Icon="home",
+                            IconClass = "text-warning",
+                            Title = "New login",
+                            Message = "Your account was logged in from a new device.",
+                            TriggerTime = DateTime.Now - TimeSpan.FromMinutes(15)
+                        },
+                        new Notification
+                        {
+                            Icon = "user-plus",
+                            IconClass = "text-success",
+                            Title = "New follower",
+                            Message = "You have a new follower.",
+                            TriggerTime = DateTime.Now - TimeSpan.FromMinutes(30)
+                        }
+                    ],
+                    ViewAllLink = new Link
+                    {
+                        Href = "#",
+                        Text = "View all notifications"
+                    }
+                },
                 LanguagesDropdown = new LanguagesDropdownViewModel(),
                 UserDropdown = new UserDropdownViewModel()
             },
--- src/Aiursoft.UiStack/Views/Shared/Components/NotificationsDropdown/NotificationsDropdownViewModel.cs
+++ src/Aiursoft.UiStack/Views/Shared/Components/NotificationsDropdown/NotificationsDropdownViewModel.cs
@@ -1,6 +1,20 @@
+using Aiursoft.UiStack.Views.Shared.Components.MegaMenu;
+
 namespace Aiursoft.UiStack.Views.Shared.Components.NotificationsDropdown;
 
 public class NotificationsDropdownViewModel
 {
+    public IReadOnlyCollection<Notification> Notifications { get; init; } = [];
     
+    public required Link ViewAllLink { get; init; }
+}
+
+public class Notification
+{
+    public required string Title { get; init; }
+    public required string Message { get; init; }
+    public required DateTime TriggerTime { get; init; }
+
+    public required string Icon { get; init; } = "bell";
+    public required string IconClass { get; init; } = "text-warning";
 }

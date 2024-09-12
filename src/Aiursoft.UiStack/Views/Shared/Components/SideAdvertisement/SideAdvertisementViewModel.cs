312525da008686c300a16505bcb3057a748fe32a
Anduin Xue <anduin@aiursoft.com>
2024-09-12 11:10:40 +0000

    Remove UiStack component views and related files
    Deleted various UiStack component views and project files from the repository. This includes the removal of component view models, csproj file, and associated helper methods.

--- src/Aiursoft.UiStack/Views/Shared/Components/SideAdvertisement/SideAdvertisementViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Aiursoft.UiStack.Views.Shared.Components.SideAdvertisement;
-
-public class SideAdvertisementViewModel
-{
-    public required string Title { get; init; }
-    public required string Description { get; init; }
-    public required string Href { get; init; }
-    public required string ButtonText { get; init; }
-}

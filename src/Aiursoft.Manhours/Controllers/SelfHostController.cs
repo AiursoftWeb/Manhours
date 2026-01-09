e9357eb58fe656d4fe42a1fc20a830840d9ed689
Anduin Xue <anduin@aiursoft.com>
2026-01-09 12:32:55 +0000

    git config pull.rebase false

---
+++ src/Aiursoft.Manhours/Controllers/SelfHostController.cs
@@ -0,0 +1,25 @@
+using Aiursoft.Manhours.Models.SelfHostViewModels;
+using Aiursoft.Manhours.Services;
+using Aiursoft.UiStack.Layout;
+using Aiursoft.UiStack.Navigation;
+using Aiursoft.WebTools.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aiursoft.Manhours.Controllers;
+
+[LimitPerMin]
+public class SelfHostController : Controller
+{
+    [RenderInNavBar(
+        NavGroupName = "Deployment",
+        NavGroupOrder = 2,
+        CascadedLinksGroupName = "Self Host",
+        CascadedLinksIcon = "server",
+        CascadedLinksOrder = 1,
+        LinkText = "Self host a new server",
+        LinkOrder = 1)]
+    public IActionResult Index()
+    {
+        return this.StackView(new IndexViewModel());
+    }
+}
---
+++ src/Aiursoft.Manhours/Models/SelfHostViewModels/IndexViewModel.cs
@@ -0,0 +1,11 @@
+using Aiursoft.UiStack.Layout;
+
+namespace Aiursoft.Manhours.Models.SelfHostViewModels;
+
+public class IndexViewModel : UiStackLayoutViewModel
+{
+    public IndexViewModel()
+    {
+        PageTitle = "Self Host";
+    }
+}
